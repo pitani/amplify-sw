4aa0d468ce98e788be0eab62fe8ea62af1008732
pitani <pitani@users.noreply.github.com>
2021-08-12 14:52:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Here
+          Learn React Here For Free
         </a>
       </header>
     </div>
